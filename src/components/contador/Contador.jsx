c81ee1497e389adac2c5d7159830f5128fa770c2
Luana Kunzler <luanakdelima@gmail.com>
2021-07-25 23:34:17 -0300

    Versão do Segundo vídeo do Projeto Aulão React

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -1,35 +1,44 @@
 import React, { Component } from "react";
+import "./Contador.css";
+import Display from "./Display";
+import PassoForm from "./PassoForm";
+import Botoes from "./Botoes";
 
 export default class Contador extends Component {
+  state = {
+    passo: this.props.passo || 1,
+    valor: this.props.valor || 0,
+  };
 
-    state = {
-        passo: this.props.passo || 1,
-        valor: this.props.valor || 0,
-    };
+  inc = () => {
+    this.setState({
+      valor: this.state.valor + this.state.passo,
+    });
+  };
 
-    inc = () => {
-        this.setState({
-            valor: this.state.valor + this.state.passo
-        })    
-    }
+  dec = () => {
+    this.setState({
+      valor: this.state.valor - this.state.passo,
+    });
+  };
 
-    dec = () => {
-        this.setState({
-            valor: this.state.valor - this.state.passo
-        }) 
-    }
+  mudarPasso = (novoPasso) => {
+    this.setState({
+      passo: novoPasso,
+    });
+  };
 
   render() {
     return (
       <div>
         <h2>Contador</h2>
-        <h4>Passo: {this.state.passo}</h4>
-        <h4>Valor: {this.state.valor}</h4>
 
-        <div>
-          <button onClick={this.inc}>+</button>
-          <button onClick={this.dec}>-</button>
-        </div>
+        <PassoForm
+          passo={this.state.passp}
+          onPassoChange={this.mudarPasso}
+        ></PassoForm>
+        <Display valor={this.state.valor}></Display>
+        <Botoes onInc={this.inc} onDec={this.dec}></Botoes>
       </div>
     );
   }
